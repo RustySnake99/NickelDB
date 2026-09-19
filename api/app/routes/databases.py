a6bc4fc91db703980cf226620dcdcf2f879d370f
RustySnake99 <narender.rudraksh99@gmail.com>
2026-09-19 10:56:29 +0530

    Structure bug fixed

--- api/app/routes/databases.py
+++ api/app/routes/databases.py
@@ -1,12 +1,13 @@
-from pathlib import Path
+from os.path import dirname, exists
+from os import makedirs
 from uuid import uuid4
 from fastapi import APIRouter, UploadFile, HTTPException
 import httpx
 from pydantic import BaseModel
 
+p = dirname(__file__)
 router = APIRouter(prefix="/databases")
-DATABASE_DIR = Path("../../data/databases")
-DATABASE_DIR.mkdir(parents=True, exist_ok=True)
+DATABASE_DIR = f"{p}/../../../databases"
 RUST_ENGINE_URL = "http://127.0.0.1:9000"
 
 class QueryRequest(BaseModel):
@@ -20,11 +21,11 @@ async def upload_database(file: UploadFile):
         raise HTTPException(status_code=400, detail="File is not an SQLite database!")
 
     database_id = str(uuid4())
-    database_dir = DATABASE_DIR / database_id
-    database_dir.mkdir()
+    database_dir = f"{DATABASE_DIR}/{database_id}"
+    makedirs(database_dir)
 
-    database_path = database_dir / "database.db"
-    with database_path.open("wb") as output:
+    database_path = f"{database_dir}/database.db"
+    with open(database_path, "wb") as output:
         while chunk := await file.read(1024 * 1024):
             output.write(chunk)
 
@@ -32,12 +33,12 @@ async def upload_database(file: UploadFile):
 
 @router.get("/{database_id}/tables")
 async def get_tables(database_id: str):
-    database_path = DATABASE_DIR / database_id / "database.db"
-    if not database_path.exists():
+    database_path = f"{DATABASE_DIR}/{database_id}/database.db"
+    if not exists(database_path):
         raise HTTPException(status_code=400, detail="Database not found!")
 
     async with httpx.AsyncClient() as client:
-        response = await client.get(f"{RUST_ENGINE_URL}/tables", params={"path": str(database_path)})
+        response = await client.get(f"{RUST_ENGINE_URL}/tables", params={"path": database_path})
     if response.status_code != 200:
         raise HTTPException(status_code=500, detail="Intenal Error occurred!")
 
@@ -45,12 +46,12 @@ async def get_tables(database_id: str):
 
 @router.post("/{database_id}/query")
 async def execute_query(database_id: str, request: QueryRequest):
-    database_path = DATABASE_DIR / database_id / "database.db"
-    if not database_path.exists():
+    database_path = f"{DATABASE_DIR}/{database_id}/database.db"
+    if not exists(database_path):
         raise HTTPException(status_code=404, detail="Database not found....")
 
     async with httpx.AsyncClient() as client:
-        response = await client.get(f"{RUST_ENGINE_URL}/query", params={"path": str(database_path), "sql": request.sql})
+        response = await client.get(f"{RUST_ENGINE_URL}/query", params={"path": database_path, "sql": request.sql})
         if response.status_code != 200:
             raise HTTPException(status_code=500, detail=response.text)
 
